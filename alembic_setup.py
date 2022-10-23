cf7d807b4115f78349b362f19a123dbc8b98a50b
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-23 14:12:29 -0400

    update automation

--- alembic_setup.py
+++ alembic_setup.py
@@ -5,15 +5,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--user', dest='user', type=str)
 parser.add_argument('--password', dest='password', type=str)
-parser.add_argument('--host', dest='host', type=str)
 parser.add_argument('--database', dest='database', type=str)
 
 args = parser.parse_args()
 user = args.user
 password = args.password
-host = args.host
+host = os.environ.get("DB_HOST", "localhost")
 database = args.database
-
 config = configparser.ConfigParser()
 config.sections()
 config.read('alembic.ini')
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -8,10 +8,14 @@ firebase = Firebase()
 firebase.setServiceAccountPath(r"./models/static/ServiceAccount.json")
 firebase.init_app()
 
+user = os.environ.get("DB_USER", "root")
+password = os.environ.get("DB_PASS", "root")
+host = os.environ.get("DB_HOST", "host.docker.internal")
+db = DB(user, password, host, database)
+repository = Repository(db)
+
 @pytest.mark.asyncio
 async def test_Lease_Service_returns_error_when_data_does_not_exist():
-    db = DB("test", "homeowner", "localhost", "roomr")
-    repository = Repository(db)
     with open(r"./tests/landlord_address_test.json", mode="r") as landlord_address_test:
         landlordAddressData = json.load(landlord_address_test)
     landlordAddress = LandlordAddress(**landlordAddressData)
@@ -21,7 +25,5 @@ async def test_Lease_Service_returns_error_when_data_does_not_exist():
 
 @pytest.mark.asyncio
 async def test_Lease_Service_returns_empty_list_when_retieving_multiple_lease_by_house_id():
-    db = DB("test", "homeowner", "localhost", "roomr")
-    repository = Repository(db)
     monad = await repository.get_lease(["0"])
     assert monad.get_param_at(0) == []

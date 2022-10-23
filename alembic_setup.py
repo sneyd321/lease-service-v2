8d9188ff6cd29f0ed2c61b64d59efc33c366507e
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-23 14:54:38 -0400

    update

--- alembic_setup.py
+++ alembic_setup.py
@@ -7,11 +7,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--user', dest='user', type=str)
 parser.add_argument('--password', dest='password', type=str)
 parser.add_argument('--database', dest='database', type=str)
+parser.add_argument('--host', dest='host', type=str)
 
 args = parser.parse_args()
 user = args.user
 password = args.password
-host = os.environ.get("DB_HOST", "localhost")
+host = args.host
 database = args.database
 
 
